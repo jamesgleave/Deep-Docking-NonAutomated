eec92b595f8ac858183736f8d43f6ce887c7a926
Francesco Gentile <gentile.francesco89@gmail.com>
2022-01-26 10:14:36 -0800

    Update simple_job_models.py
    fixed good_mol

--- phase_2-3/simple_job_models.py
+++ phase_2-3/simple_job_models.py
@@ -111,6 +111,7 @@ with open(DATA_PATH+'/iteration_'+str(1)+'/validation_labels.txt','r') as ref:
 scores_val = np.array(scores_val)
 
 first_mols = int(100*t_mol/13) if percent_first_mols == -1.0 else int(percent_first_mols * len(scores_val))
+last_mols = 100 if percent_last_mols == -1.0 else int(percent_last_mols * len(scores_val))
 
 if n_it==1:
     # 'good_mol' is the number of top scoring molecules to save at the end of the iteration
@@ -121,7 +122,7 @@ else:
     elif polynomial_dec != -1:
         good_mol = int()
     else:
-        good_mol = int(((100-first_mols)*n_it + titr*first_mols-100)/(titr-1))     # linear decrease as interations increase
+        good_mol = int(((last_mols-first_mols)*n_it + titr*first_mols-last_mols)/(titr-1))     # linear decrease as interations increase
 
 print(isl)
 # If this is the last iteration then we save only 100 molecules
